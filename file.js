4645d9ee765c93a561f95e10f842b1252cd8c86d
NatanLuiz <34273197+natanluiz99@users.noreply.github.com>
2023-11-14 19:40:15 -0300

    Update file.js

--- file.js
+++ file.js
@@ -1,1 +1,6 @@
-
+new gridjs.Grid({
+  columns: ["Nome", "Status", "Missão delegada"],
+  data: [
+    ["João Guilherme", "Vivo", "Mantiqueira"],
+  ]
+}).render(document.getElementById("wrapper"));
