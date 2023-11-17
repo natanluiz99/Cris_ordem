3ab242b3bbd09a3f51cd6377487a8fadf1d8ac32
NatanLuiz <34273197+natanluiz99@users.noreply.github.com>
2023-11-16 23:54:53 -0300

    Update file.js

--- file.js
+++ file.js
@@ -1,24 +1,31 @@
+
+
 new gridjs.Grid({
-    columns: ["Nome", "Status", "Missão delegada"],
-    data: [["João Guilherme", "Vivo", "Mantiqueira"]]
-  }).render(document.getElementById("diva"));
-  
-  function convertBinary() {
-    var output = document.getElementById("outputBinary");
-    var input = document.getElementById("inputBinary").value;
-    output.value = "";
-    for (i = 0; i < input.length; i++) {
-      var e = input[i].charCodeAt(0);
-      var s = "";
-      do {
-        var a = e % 2;
-        e = (e - a) / 2;
-        s = a + s;
-      } while (e != 0);
-      while (s.length < 8) {
-        s = "0" + s;
-      }
-      output.value += s;
+  columns: ["Nome", "Status", "Missão delegada"],
+  data: [["João Guilherme", "Vivo", "Mantiqueira"], ["Augusto souza", "Vivo", "Mantiqueira"], ["Victória pantoja", "Vivo", "Mantiqueira"]],
+
+}).render(document.getElementById("diva"));
+
+
+
+
+
+
+function convertBinary() {
+  var output = document.getElementById("outputBinary");
+  var input = document.getElementById("inputBinary").value;
+  output.value = "";
+  for (i = 0; i < input.length; i++) {
+    var e = input[i].charCodeAt(0);
+    var s = "";
+    do {
+      var a = e % 2;
+      e = (e - a) / 2;
+      s = a + s;
+    } while (e != 0);
+    while (s.length < 8) {
+      s = "0" + s;
     }
+    output.value += s;
   }
-  
+}
